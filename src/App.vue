f9c0618c5bbd62d07084416969b02cdc6a8937cd
SamRong <Yungsun3307@163.com>
2021-03-20 17:36:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>关于组件数据通信的demo</h1>
+    <div class="sample01">
+      <h2>组件内值变化</h2>
+      <Sample01 />
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+import Sample01 from './components/Sample01.vue';
+export default {
+  name: 'App',
+  components: { Sample01 },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.sample01 {
+  text-align: left;
+}
+</style>
---
+++ src/components/Sample01.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>n: {{ n }}</h3>
+    <button @click="add">n+1</button>
+    <button @click="reduce">n-1</button>
+    <button @click="reset">reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      n: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.n += 1;
+    },
+    reduce() {
+      this.n -= 1;
+    },
+    reset() {
+      this.n = 0;
+    },
+  },
+};
+</script>
